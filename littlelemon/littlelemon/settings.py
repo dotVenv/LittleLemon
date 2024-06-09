11893388fc8bf8d6c40ffbcb9bb19f29eff64ed4
dotVenvDev <dotVenv@skiff.com>
2024-06-09 02:59:31 -0500

    database setup

--- littlelemon/littlelemon/settings.py
+++ littlelemon/littlelemon/settings.py
@@ -78,9 +78,20 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+    },
+    'mydb': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'restaurant',
+        'USER':'root',
+        'PASSWORD': 'junior1233!',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+            
+        }
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
